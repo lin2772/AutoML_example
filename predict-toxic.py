6e102befdbf0461c632c6cacd5ac7cd14ed23de9
Weilie <wl223@duke.edu>
2022-03-25 03:16:01 +0000

    change format

--- predict-toxic.py
+++ predict-toxic.py
@@ -26,16 +26,16 @@ df = pd.DataFrame(data=my_data)
 
 
 def create_tf_serving_json(data):
-  return {'inputs': {name: data[name].tolist() for name in data.keys()} if isinstance(data, dict) else data.tolist()}
+    return {'inputs': {name: data[name].tolist() for name in data.keys()} if isinstance(data, dict) else data.tolist()}
 
 def score_model(dataset):
-  url = 'https://adb-4167085237468301.1.azuredatabricks.net/model/toxic_best_accuracy/1/invocations'
-  headers = {'Authorization': f'Bearer {os.environ.get("DATABRICKS_TOKEN")}'}
-  data_json = dataset.to_dict(orient='split') if isinstance(dataset, pd.DataFrame) else create_tf_serving_json(dataset)
-  response = requests.request(method='POST', headers=headers, url=url, json=data_json)
-  if response.status_code != 200:
-    raise Exception(f'Request failed with status {response.status_code}, {response.text}')
-  return response.json()
+    url = 'https://adb-4167085237468301.1.azuredatabricks.net/model/toxic_best_accuracy/1/invocations'
+    headers = {'Authorization': f'Bearer {os.environ.get("DATABRICKS_TOKEN")}'}
+    data_json = dataset.to_dict(orient='split') if isinstance(dataset, pd.DataFrame) else create_tf_serving_json(dataset)
+    response = requests.request(method='POST', headers=headers, url=url, json=data_json)
+    if response.status_code != 200:
+        raise Exception(f'Request failed with status {response.status_code}, {response.text}')
+    return response.json()
 
 
 if __name__ == "__main__":
